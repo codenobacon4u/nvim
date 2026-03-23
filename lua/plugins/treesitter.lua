f37d56697f4d061e88266284da781a8e66dc58af
codenobacon4u <kralcmit@gmail.com>
2026-03-22 20:56:47 -0400

    Fix tresitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "java", "lua", "vim", "vimdoc", "bash", "markdown", "markdown_inline" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.config").setup({
+			ensure_installed = { "c", "java", "lua", "vimdoc", "bash", "markdown", "markdown_inline" },
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+		})
+	end,
 }
