53cc59a9e9d8e00429c56a7998217e149b3bb569
codenobacon4u <kralcmit@gmail.com>
2026-05-01 20:46:43 -0400

    Fix telescope error

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,37 +1,34 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  cmd = "Telescope",
-  opt = {
-    defaults = {
-      prompt_prefix = "   ",
-      selection_caret = " ",
-      entry_prefix = " ",
-      sorting_strategy = "ascending",
-      layout_config = {
-        horizontal = {
-          prompt_position = "top",
-          preview_width = 0.55,
-        },
-        width = 0.87,
-        height = 0.80,
-      },
-      mappings = {
-        n = { ["q"] = require("telescope.actions").close },
-      },
-    },
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	cmd = "Telescope",
+	opt = {
+		defaults = {
+			prompt_prefix = "   ",
+			selection_caret = " ",
+			entry_prefix = " ",
+			sorting_strategy = "ascending",
+			layout_config = {
+				horizontal = {
+					prompt_position = "top",
+					preview_width = 0.55,
+				},
+				width = 0.87,
+				height = 0.80,
+			},
+		},
 
-    extensions_list = { "themes", "terms" },
-    extensions = {},
-  },
-  config = function()
-    require('telescope').setup({})
-    
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-  end
+		extensions_list = { "themes", "terms" },
+		extensions = {},
+	},
+	config = function()
+		require("telescope").setup({})
+
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	end,
 }
